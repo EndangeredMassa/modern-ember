c430c498314d157074b07cec6e00b446c85ae407
Sean Massa <endangeredmassa@gmail.com>
2019-03-01 17:06:34 -0600

    setup template lint rules

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+  rules: {
+    'no-implicit-this': true,
+    'attribute-indentation': {
+      'element-open-end': 'last-attribute',
+      'mustache-open-end': 'last-attribute'
+    },
+    'eol-last': 'never',
+    'link-href-attributes': true,
+    'no-action-modifiers': true,
+    'no-extra-mut-helper-argument': true,
+    'no-trailing-spaces': true,
+    'template-length': false,
+    'deprecated-each-syntax': true,
+    'deprecated-inline-view-helper': true,
+    'deprecated-render-helper': true
+  }
 };

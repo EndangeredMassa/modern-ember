d8f6e30220127984b1fc1349340580a79467a6f8
Sean Massa <endangeredmassa@gmail.com>
2019-03-01 19:00:52 -0600

    add a11y testing

---
+++ tests/acceptance/a11y-audit-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import a11yAudit from 'ember-a11y-testing/test-support/audit';
+
+function audit(path) {
+  test(`audit ${path}`, async function(assert) {
+    await visit(path);
+    await a11yAudit();
+    assert.expect(0);
+  });
+}
+
+module('Acceptance | a11y audit', function(hooks) {
+  setupApplicationTest(hooks);
+
+  audit('/');
+});

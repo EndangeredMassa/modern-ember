45a2e89e241a6c7f92c0ca50ff45bf243693218e
Sean Massa <endangeredmassa@gmail.com>
2019-03-02 01:48:21 -0600

    add percy

---
+++ tests/acceptance/visual-regression-test.js
@@ -0,0 +1,18 @@
+import { module, test } from 'qunit';
+import { visit } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import {percySnapshot} from 'ember-percy';
+
+function screenshot(path) {
+  test(`screenshot ${path}`, async function(assert) {
+    await visit(path);
+    await percySnapshot(assert);
+    assert.expect(0);
+  });
+}
+
+module('Acceptance | visual regression testing', function(hooks) {
+  setupApplicationTest(hooks);
+
+  screenshot('/');
+});

cd21cd5eecf5b9ac951a8443177eed266fe95049
Sean Massa <endangeredmassa@gmail.com>
2019-03-05 15:58:55 -0600

    ignore build-time addons in dependency linting

--- config/dependency-lint.js
+++ config/dependency-lint.js
@@ -1,5 +1,13 @@
 'use strict';
 
-module.exports = {
+// You can specify certain addons to be allowed
+// to have multiple versions here.
+// This should only be done for addons that do
+// something at build-time.
 
+module.exports = {
+  allowedVersions: {
+    'ember-decorators': '*',
+    '@ember-decorators/utils': '*'
+  }
 };
